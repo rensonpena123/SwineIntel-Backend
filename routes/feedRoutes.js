212ed2df776506b8ae608a1ddaa2c4f61e709f8b
rensonpena123 <rensonpenag@gmail.com>
2026-05-21 21:48:52 +0800

    edited the feedRoutes.js - it only allows Owner to restock feeds

--- routes/feedRoutes.js
+++ routes/feedRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { getFeedInventory, restockFeed, withdrawFeed } from '../controllers/feedController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect, ownerOnly } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/')
   .get(protect, getFeedInventory);
 
-router.post('/restock', protect, restockFeed);
+router.post('/restock', protect, ownerOnly, restockFeed);
 router.put('/withdraw', protect, withdrawFeed);
 
 export default router;
